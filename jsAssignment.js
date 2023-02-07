981ad68fbebc3c5417f6112c8071f8a71b03ffb0
muhib-kagozi <muhib.kagozi@gmail.com>
2023-02-07 20:02:08 +0600

    problem 5 done

--- jsAssignment.js
+++ jsAssignment.js
@@ -27,5 +27,57 @@ console.log(stingName);
 
 /* 
 Problem no:03
-
+input any digit and minus 7. if it is less than 7 then return the result, other wise return double of input number.
 */
+function isLGSeven(userNUmber) {
+    const afterMinus = userNUmber - 7;
+    if (afterMinus >= 7) {
+        return userNUmber * 2;
+    }
+    else {
+        return afterMinus;
+    }
+}
+const userInput = isLGSeven(15);
+console.log(userInput);
+
+/* 
+Problem no: 04
+input an array and find out if there is any badData and finally return the total count of badData. 
+*/
+function findingBadData(userArray) {
+    const badData = [];
+    for (let i = 0; i < userArray.length; i++) {
+        const element = userArray[i];
+        if (element < 0) {
+            badData.push(element);
+        }
+    }
+    return badData.length;
+}
+const age = [12, -43, 22, -92, -22, -60, -40, 54, 33];
+const fullArray = findingBadData(age);
+console.log(fullArray);
+
+/* 
+Problem no: 05
+take three parameters and multiply by 21, 32 and 43 successively, then add three result. if the result is more than 2000, then minus 2000 from result and return it, otherwise just return the result.
+*/
+function gemsToDiamond(first, second, third) {
+    const firstMulti = first * 21;
+    const secondMulti = second * 32;
+    const thirdMulti = third * 43;
+    const total = firstMulti + secondMulti + thirdMulti;
+    if (total > 2000) {
+        const afterMinus = total - 2000;
+        return afterMinus;
+    }
+    else {
+        return total;
+    }
+}
+const firstGems = 1;
+const secondGems = 1;
+const thirdGems = 1;
+const allGems = gemsToDiamond(firstGems, secondGems, thirdGems);
+console.log(allGems);
